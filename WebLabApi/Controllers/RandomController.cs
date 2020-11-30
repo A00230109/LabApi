37d90c591f35ef787031b045c935e9f92cc9c08e
Youssef Nafaa <nafaayoussef@gmail.com>
2020-11-30 11:55:01 -0500

    Lab API

---
+++ WebLabApi/Controllers/RandomController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebLabApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RandomController : ControllerBase
+    {
+        public Random rnd;
+
+
+        [HttpGet]
+        public int Get()
+        {
+            rnd = new Random();
+            double flt = rnd.NextDouble();
+            int shift = Convert.ToInt32(Math.Floor(25 * flt));
+            return shift;
+        }
+
+        [HttpPost]
+        public double Post()
+        {
+            rnd = new Random();
+            double flt = rnd.NextDouble();
+
+            return flt;
+        }
+
+        [HttpPut]
+        public string Put()
+        {
+            return "Thank you for the new number!";
+        }
+
+
+        [HttpDelete]
+        public char Delete()
+        {
+            rnd = new Random();
+            double flt = rnd.NextDouble();
+            int shift = Convert.ToInt32(Math.Floor(25 * flt));
+
+            char letter = Convert.ToChar(shift + 65);
+            return letter;
+        }
+
+        [HttpPatch]
+        public string Patch()
+        {
+            DateTime dateTime = DateTime.Now;
+            string sDateTimeNow = dateTime.ToString();
+            return sDateTimeNow;
+        }
+
+        [HttpOptions]
+        public dynamic Option()
+        {
+            Response.ContentType = "application/json";
+            var json = new { GET = "Returns a random integer",
+                             POST = "Returns a random double",
+                             PUT = "Returns a string 'Thank you for the new number!'",
+                             DELETE = "Returns a random character",
+                             PATCH = "Returns the current date"
+                           };
+            return json;
+        }
+    }
+}
